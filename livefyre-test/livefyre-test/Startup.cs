235a1eaefba827bfb2f68773cd58795eb7342f2d
khrubes <khrubes@iastate.edu>
2017-07-11 13:00:11 -0700

    add solution template

---
+++ livefyre-test/livefyre-test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(livefyre_test.Startup))]
+namespace livefyre_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
